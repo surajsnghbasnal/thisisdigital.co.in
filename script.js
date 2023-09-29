d39d71bbb2fb659b2a85963d70c7f5ee20236018
Suraj <surajsngbasnal16@gmail.com>
2023-09-29 13:15:53 +0530

    first commit

--- script.js
+++ script.js
@@ -280,7 +280,7 @@ gsap.from('.post', {
     }
 })
 
-var swiper = new Swiper(".mySwiper2", {
+var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
@@ -288,7 +288,7 @@ var swiper = new Swiper(".mySwiper2", {
         disableOnInteraction: false,
     },
     pagination: {
-        el: ".swiper-pagination2",
+        el: ".swiper-pagination",
         clickable: true,
     },
     grabCursor: true,
